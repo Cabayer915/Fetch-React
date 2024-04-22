f7a6717170051c59aee39f6674cc42c9e9970675
Cabayer915 <cauaribeirob915@gmail.com>
2024-04-22 04:17:47 -0300

    Adicionando Assets Visuais do GitFind

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Styles from './styles.css';
 import App from './pages/home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
